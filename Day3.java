c61548bf740e9d8e3a5ebb5cfd75c2beed81675d
gugashreya09 <gugashreya09@gmail.com>
2026-05-24 20:26:14 +0530

    getting input for three variables

---
+++ Day3.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class Day3{
+    public static void main(String args[])
+    {
+        Scanner Scan = new Scanner(System.in);
+        String name = Scan.nextLine();
+        int age = Scan.nextInt();
+        Scan.nextLine();
+        String address = Scan.nextLine();
+        System.out.println("My name is"+name);
+        System.out.println("My age is"+age);
+        System.out.println("My address is"+address);
+        Scan.close();
+
+    }
+}
