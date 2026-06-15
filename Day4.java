ae1894e2145078232451843db0e4d889d508c0d4
gugashreya09 <gugashreya09@gmail.com>
2026-06-15 16:24:53 +0530

    comparing two strings

---
+++ Day4.java
@@ -0,0 +1,13 @@
+class Day4{
+    public static void main(String args[])
+    {
+        String f1 = new String("apple");
+         String f2 = new String("apple");
+         String a1 = "orange";
+         String a2 = "orange";
+         System.out.print(f1==f2);
+         System.out.print(a1==a2);
+
+    }
+
+}
