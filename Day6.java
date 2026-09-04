d71b7c8568ac92e1192fb8d795aff90568f2d792
gugashreya09 <gugashreya09@gmail.com>
2026-09-04 18:33:18 +0530

    second largest number

---
+++ Day6.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+public class Day6 {
+    public static void main(String args[])
+    {    Scanner sc=new Scanner(System.in);
+    
+       int[] arr=new int[5];
+       
+       for(int i=0;i<5;i++)
+       {
+           arr[i]=sc.nextInt();
+       }
+       int largest=arr[0];
+       int second=arr[0];
+
+           for(int i=1;i<5;i++)
+           {
+            if(arr[i]>largest)
+            {
+                second=largest;
+                largest=arr[i];
+            }
+            else if(arr[i]>second && arr[i]!=largest)
+            {
+                second=arr[i];
+            }
+        }
+              System.out.println(second);
+          
+
+       }
+    }
+
+    
